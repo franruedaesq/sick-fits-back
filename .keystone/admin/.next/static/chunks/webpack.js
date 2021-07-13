1d1cca85c0678544320aa63a8c9fbc06b68c2834
franruedaesq <franruedaesq@gmail.com>
2021-07-13 12:26:42 -0300

    support for password reset

--- .keystone/admin/.next/static/chunks/webpack.js
+++ .keystone/admin/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e52ebb47f978d72e5a53";
+/******/ 	var hotCurrentHash = "96237b997a9f7d191494";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
